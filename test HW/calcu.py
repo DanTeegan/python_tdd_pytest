f966269e552dbc7a5ffd4e5950c67de5de749af1
dan. teegan <dan.teegan@hotmail.com>
2020-07-07 20:21:10 +0100

    Test homework 07/07/2020

--- test HW/calcu.py
+++ test HW/calcu.py
@@ -1,11 +1,20 @@
-
+# Here we are importing math into our file
 import math
+# Here we are creating a class called Calcu
 class Calcu:
-
-    def find_sqrt(num1, num2):
+    # Here we make a method to work out the sq root
+    def find_sqrt(self, num2):
         return math.sqrt(num2)
-
-    def find_ceil(num1, num2):
+    # Here we make a method to round up
+    def find_ceil(self, num2):
         return math.ceil(num2)
+    # Here we make a method to round down
+    def find_floor(self, num2):
+        return math.floor(num2)
 
+# Here we are creating an object from the Calcu class
 simple_calcu = Calcu()
+
+# Here we are just printing using the methods created to see if they work.
+print(simple_calcu.find_ceil(102.8))
+print(simple_calcu.find_floor(1001.4))
--- test HW/calcu_test.py
+++ test HW/calcu_test.py
@@ -1,13 +1,26 @@
+# Here we are importing the class Calcu from the calcu file
 from calcu import Calcu
+
+# Here we are importing pytest
 import pytest
+
+# Here we are importing unittest
 import unittest
 
+# Here we create a class called Calcu_test. We have placed unittest.TestCase in order to run a test
 class Calcu_Test(unittest.TestCase):
 
+    # Here we are defining our object of Calcu which referances the object made in the calcu file
     simple_calcu = Calcu()
 
+    # Here we have a test to check the sq root using the method from the Calcu class
     def test_sqrt(self):
         self.assertEqual(self.simple_calcu.find_sqrt(16), 4)
 
+    # Here we have a test to check the round up function ceil. Again a method from the Calcu class
     def test_ceil(self):
         self.assertEqual(self.simple_calcu.find_ceil(9.8), 10)
+
+    # Here we have a test to check the round down function floor. Again a method from the Calcu class
+    def test_floor(self):
+        self.assertEqual(self.simple_calcu.find_floor(1032.32), 1032)
